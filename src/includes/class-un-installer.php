ab1e80878605e9b030ae1f2bcebf37a514f26a06
JDGrimes <jdg@codesymphony.co>
2017-03-30 11:48:25 -0400

    Create blocks databases on install
    - Adds a database for the blocks.
    - Adds a database for the block logs.
    
    See #4

---
+++ src/includes/class-un-installer.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Un/installer class.
+ *
+ * @package WordPoints_Top_Users_In_Period
+ * @since   1.0.0
+ */
+
+/**
+ * Installs, uninstalls, and updates the module.
+ *
+ * @since 1.0.0
+ */
+class WordPoints_Top_Users_In_Period_Un_Installer
+	extends WordPoints_Un_Installer_Base {
+
+	/**
+	 * @since 1.0.0
+	 */
+	protected $type = 'module';
+
+	/**
+	 * @since 1.0.0
+	 */
+	protected $schema = array(
+		'global' => array(
+			'tables' => array(
+				'wordpoints_top_users_in_period_blocks' => '
+					id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+					block_type CHAR(32) NOT NULL,
+					start_date DATETIME NOT NULL,
+					end_date DATETIME NOT NULL,
+					query_signature CHAR(64) NOT NULL,
+					PRIMARY KEY  (id),
+					KEY block_signature (block_type,query_signature(8))',
+				'wordpoints_top_users_in_period_block_logs' => '
+					id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+					block_id BIGINT(20) UNSIGNED NOT NULL,
+					user_id BIGINT(20) UNSIGNED NOT NULL,
+					points BIGINT(20) UNSIGNED NOT NULL,
+					PRIMARY KEY  (id),
+					KEY block_user_id (user_id,block_id)',
+			),
+		),
+	);
+}
+
+return 'WordPoints_Top_Users_In_Period_Un_Installer';
+
+// EOF
---
+++ tests/phpunit/test-uninstall.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Uninstall test case.
+ *
+ * @package WordPoints_Top_Users_In_Period
+ * @since 1.0.0
+ */
+
+/**
+ * Tests uninstalling the module.
+ *
+ * @since 1.0.0
+ */
+class Top_Users_In_Period_Uninstall_Test
+	extends WordPoints_PHPUnit_TestCase_Module_Uninstall {
+
+	/**
+	 * Test installation and uninstallation.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_uninstall() {
+
+		global $wpdb;
+
+		/*
+		 * First test that the module installed itself properly.
+		 */
+
+		$this->assertTableExists( $wpdb->base_prefix . 'wordpoints_top_users_in_period_blocks' );
+		$this->assertTableExists( $wpdb->base_prefix . 'wordpoints_top_users_in_period_block_logs' );
+
+		/*
+		 * Now, test that it uninstalls itself properly.
+		 */
+
+		$this->uninstall();
+
+		// Check that everything with this module's prefix has been uninstalled.
+		$this->assertUninstalledPrefix( 'wordpoints_top_users_in_period' );
+	}
+}
+
+// EOF
